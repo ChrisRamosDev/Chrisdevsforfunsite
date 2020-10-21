9a3431a9c279d46b0916915e8bf17e4e166f76e3
Christopher Ramos <25787207+Cramos711@users.noreply.github.com>
2020-10-21 01:12:38 -0400

    config wpgraphql and added blog to site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,31 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Video from "../components/video"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 className="text-center">Welcome!</h1>
-    <Video />
-    <p className="[ text-center text-xl ]">
-      <span role="img" aria-label="construction-sign">
-        🚧
-      </span>{" "}
-      Apologies for the mess, this site is under construction.{" "}
-      <span role="img" aria-label="construction-sign">
-        🚧
-      </span>
-    </p>
-    <p className="[ text-center text-lg ]">Please check back again soon!</p>
-  </Layout>
-)
+const Home = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1 className="text-center">Welcome!</h1>
+      <Video />
+      <p className="[ text-center text-xl ]">
+        <span role="img" aria-label="construction-sign">
+          🚧
+        </span>{" "}
+        Apologies for the mess, this site is under construction.{" "}
+        <span role="img" aria-label="construction-sign">
+          🚧
+        </span>
+      </p>
+      <p className="[ text-center text-lg ]">
+        Please check back again soon!
+        <Link to="/page-2">&nbsp;But heres a Link to my blog</Link>
+      </p>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export default Home
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = ({ data }) => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Blog" />
+    <h1>Blog</h1>
+    <div className="blog">
+      <h3>Posts</h3>
+      {data.allWpPost.nodes.map(node => (
+        <div key={node.slug} className="blog-post flow-sm">
+          {/* highlight-start */}
+          <Link to={node.slug}>
+            <p className="blog-title">{node.title}</p>
+          </Link>
+          {/* highlight-end */}
+          <div
+            dangerouslySetInnerHTML={{ __html: node.excerpt }}
+            className="blog-content"
+          />
+        </div>
+      ))}
+    </div>
+    <div>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
   </Layout>
 )
+export const pageQuery = graphql`
+  query {
+    allWpPost(sort: { fields: date }) {
+      nodes {
+        title
+        excerpt
+        slug
+      }
+    }
+  }
+`
 
 export default SecondPage
