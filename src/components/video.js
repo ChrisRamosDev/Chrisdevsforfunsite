26a2a8bab8d895179a93cc18513ed400dbb420de
Christopher Ramos <25787207+Cramos711@users.noreply.github.com>
2020-10-20 22:38:48 -0400

    updated favicon

--- src/components/video.js
+++ src/components/video.js
@@ -6,6 +6,8 @@ export default function Video() {
   return (
     <video autoplay="" loop="" muted="muted" width="800" className="mx-auto">
       <source src={Factorymp4} type="video/mp4" />
+      <track kind="captions" srcLang="en" src="#" />
+      Sorry, your browser doesn't support this video.
     </video>
   )
 }
